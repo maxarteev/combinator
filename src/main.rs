bc4b224bb29fc17b8c297af321eb75fdfc44450a
theoryM <snake_9@tut.by>
2023-02-17 11:52:54 +0300

    read_all_files

--- src/main.rs
+++ src/main.rs
@@ -3,18 +3,34 @@
 #![allow(unused)]
 
 use std::{
-    fs::File, 
-    io::prelude::*
+    fs::{File, read}, 
+    io::{prelude::*, Error}
 };
 
+fn parse_string_from_file(str: String) {
+    for line in str.split_terminator("\r\n") {
+        println!("{:?}", line);
+    }
+}
 
-fn open_file() {
-    let mut file = File::open("F:/temp/csv/csv/1.csv");
+fn read_file(file: &mut File) -> Result<String, Error> {
+    let mut buff = String::new() ;
+    file.read_to_string(&mut buff)?;
+    Ok(buff)
+}
+fn open_file(path: &str) -> Result<File, Error> {
+    File::open(path)
+}
+fn open_files_in_dir(dir: String) {
     
-    println!("{:?}", file);
 }
 
 
 fn main() {
-    open_file()
+    let mut vec_files: Vec<File> = Vec::new();
+
+    let mut file = open_file("F:/temp/csv/csv/1.csv").expect("file not open");
+    let string_from_file = read_file(&mut file).expect("can't read file");
+    parse_string_from_file(string_from_file);
+
 }
